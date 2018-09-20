b47f7c46021055396fec2bec8db00b977762f705
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-09-20 17:51:16 +0200

    * updated for data rework

--- misaxx_kidney_glomeruli/kidney_glomeruli.h
+++ misaxx_kidney_glomeruli/kidney_glomeruli.h
@@ -17,23 +17,22 @@ using namespace coixx;
 namespace misaxx::module::kidney_glomeruli_detection {
     struct kidney_glomeruli : public misa_module_declaration {
 
-        data<misa_image_stack<images::grayscale_float >> m_input_autofluorescence = declare_data<misa_image_stack<images::grayscale_float >>("autofluorescence");
-        data<misa_image_stack<images::mask>> m_output_segmented2d = declare_data<misa_image_stack<images::mask>>("segmented2d");
-        data<misa_image_stack<images::labels >> m_output_segmented3d = declare_data<misa_image_stack<images::labels>>("segmented3d");
-        data<misa_exportable_meta_data> m_output_quantification = declare_data<misa_exportable_meta_data>("quantified");
-        misa_submodule<tissue_detection::tissue_detection_module> m_tissue_detection = declare_submodule<tissue_detection::tissue_detection_module>("tissue");
+        data<misa_image_stack<images::grayscale_float >> m_input_autofluorescence;
+        data<misa_image_stack<images::mask>> m_output_segmented2d;
+        data<misa_image_stack<images::labels >> m_output_segmented3d;
+        data<misa_exportable_meta_data> m_output_quantification;
+        misa_submodule<tissue_detection::tissue_detection_module> m_tissue_detection;
 
         void init_data() override {
-            m_input_autofluorescence->from_filesystem(filesystem.imported);
-            m_output_quantification->from_filesystem(filesystem.exported / filesystem::as<filesystem::file >("quantified.json"));
+
+            import_from_filesystem(m_input_autofluorescence, "/");
+            process(m_output_segmented2d, m_input_autofluorescence, "glomeruli2d");
+            process(m_output_segmented3d, m_output_segmented2d, "glomeruli3d");
+            export_to_filesystem(m_output_quantification, "quantified.json");
 
             // Init the submodule
             m_tissue_detection.definition().m_input_autofluorescence = m_input_autofluorescence;
-            m_tissue_detection.init(*this);
-
-            // Init dependent data
-            m_output_segmented2d->from_reference_stack(m_input_autofluorescence);
-            m_output_segmented3d->from_reference_stack(m_output_segmented2d);
+            init_submodule(m_tissue_detection, "tissue");
         }
 
         glomeruli &get_glomeruli() {
