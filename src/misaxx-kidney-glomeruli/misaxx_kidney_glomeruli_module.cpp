11a5dafe28b0295686e4990af96cea19f124ca5a
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-12-19 18:18:21 +0100

    * fix * TODO: Scheduling not working for submodules

--- src/misaxx-kidney-glomeruli/misaxx_kidney_glomeruli_module.cpp
+++ src/misaxx-kidney-glomeruli/misaxx_kidney_glomeruli_module.cpp
@@ -11,7 +11,7 @@ using namespace misaxx_kidney_glomeruli;
 void misaxx_kidney_glomeruli_module::misa_init() {
 
     // Load voxel size
-    std::cout << m_input_autofluorescence.get_ome_metadata()->dumpXML() << std::endl;
+//    std::cout << m_input_autofluorescence.get_ome_metadata()->dumpXML() << std::endl;
 
     m_voxel_size = misaxx_ome::misa_ome_voxel_size(*m_input_autofluorescence.get_ome_metadata(),
             0, misaxx_ome::misa_ome_voxel_size::ome_unit_type::MICROMETER);
@@ -19,27 +19,27 @@ void misaxx_kidney_glomeruli_module::misa_init() {
     group preprocessing;
     preprocessing << misa_dispatch(dispatch_tissue_detection);
 
-//    group segmentation2d({{preprocessing}});
-//
-//    for (size_t plane = 0; plane < m_input_autofluorescence.size(); ++plane) {
-//        auto &worker = misa_dispatch(dispatch_segmentation2d);
-//        worker.m_input_autofluoresence = m_input_autofluorescence.at(plane);
-////        worker.m_input_tissue = m_tissue_detection. // TODO
-//        worker.m_output_segmented2d = m_output_segmented2d.at(plane);
-//        segmentation2d << worker;
-//    }
-//
-//    chain work3d({segmentation2d});
-//
-//    {
-//        auto &worker = misa_dispatch(dispatch_segmentation3d);
-//        worker.m_input_segmented2d = m_output_segmented2d;
-//        worker.m_output_segmented3d = m_output_segmented3d;
-//        work3d >> worker;
-//    }
-//    {
-//        auto &worker = misa_dispatch(dispatch_quantification);
-//        worker.m_input_segmented3d = m_output_segmented3d;
-//        work3d >> worker;
-//    }
+    group segmentation2d({{preprocessing}});
+
+    for (size_t plane = 0; plane < m_input_autofluorescence.size(); ++plane) {
+        auto &worker = misa_dispatch(dispatch_segmentation2d);
+        worker.m_input_autofluoresence = m_input_autofluorescence.at(plane);
+//        worker.m_input_tissue = m_tissue_detection. // TODO
+        worker.m_output_segmented2d = m_output_segmented2d.at(plane);
+        segmentation2d << worker;
+    }
+
+    chain work3d({segmentation2d});
+
+    {
+        auto &worker = misa_dispatch(dispatch_segmentation3d);
+        worker.m_input_segmented2d = m_output_segmented2d;
+        worker.m_output_segmented3d = m_output_segmented3d;
+        work3d >> worker;
+    }
+    {
+        auto &worker = misa_dispatch(dispatch_quantification);
+        worker.m_input_segmented3d = m_output_segmented3d;
+        work3d >> worker;
+    }
 }
