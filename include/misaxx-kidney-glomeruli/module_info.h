21d4d03b478d2ea5e45235e9dc6b90edb88ed32f
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-01-16 16:37:56 +0100

    * updated to new MISA++

---
+++ include/misaxx-kidney-glomeruli/module_info.h
@@ -0,0 +1,7 @@
+#include <misaxx/misa_module_info.h>
+
+namespace misaxx_kidney_glomeruli {
+    inline misaxx::misa_module_info module_info() {
+        return misaxx::misa_module_info("misaxx-kidney-glomeruli", "0.1.0");
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <misaxx-kidney-glomeruli/misaxx_kidney_glomeruli_module.h>
 #include <misaxx/runtime/misa_cli.h>
+#include <misaxx-kidney-glomeruli/module_info.h>
 
 using namespace misaxx;
 using namespace misaxx_kidney_glomeruli;
 
 int main(int argc, const char** argv) {
-    misa_cli<misaxx_kidney_glomeruli_module> cli("misaxx-kidney-glomeruli");
+    misa_cli<misaxx_kidney_glomeruli_module> cli(misaxx_kidney_glomeruli::module_info());
     return cli.prepare_and_run(argc, argv);
 }
