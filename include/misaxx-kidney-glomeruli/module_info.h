436ceac277374d73382a1d3ba5fb957cd6845b0f
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-01-16 17:40:45 +0100

    * updated to new MISA++

--- include/misaxx-kidney-glomeruli/module_info.h
+++ include/misaxx-kidney-glomeruli/module_info.h
@@ -2,6 +2,6 @@
 
 namespace misaxx_kidney_glomeruli {
     inline misaxx::misa_module_info module_info() {
-        return misaxx::misa_module_info("misaxx-kidney-glomeruli", "0.1.0");
+        return misaxx::misa_module_info("misaxx-kidney-glomeruli", "0.1.0", "MISA++ Kidney Glomeruli Segmentation");
     }
 }
