4ba64912313d54e0e1b256b594290c1dc57cf31a
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-03-25 13:45:22 +0100

    * fix

--- src/misaxx-kidney-glomeruli/module_info.cpp
+++ src/misaxx-kidney-glomeruli/module_info.cpp
@@ -12,7 +12,7 @@ misaxx::misa_module_info misaxx_kidney_glomeruli::module_info() {
     info.add_author("Ruman Gerst");
     info.set_license("BSD-2-Clause");
     info.set_organization("Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Institute (HKI), Jena, Germany");
-    info.set_url("https://asb-git.hki-jena.de/RGerst/misaxx-tissue/");
+    info.set_url("https://asb-git.hki-jena.de/RGerst/misaxx-kidney-glomeruli/");
 
     info.add_dependency(misaxx::module_info());
     info.add_dependency(misaxx_tissue::module_info());
