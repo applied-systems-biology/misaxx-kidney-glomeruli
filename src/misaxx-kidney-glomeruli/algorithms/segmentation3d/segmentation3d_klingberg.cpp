90bfe64cdee493841305349d4548ef6ff4a3a369
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-04-11 14:43:28 +0200

    * fix?

--- src/misaxx-kidney-glomeruli/algorithms/segmentation3d/segmentation3d_klingberg.cpp
+++ src/misaxx-kidney-glomeruli/algorithms/segmentation3d/segmentation3d_klingberg.cpp
@@ -3,10 +3,22 @@
 //
 
 #include "segmentation3d_klingberg.h"
+#include <set>
 
 namespace cv::images {
     using mask = cv::Mat_<uchar>;
     using labels = cv::Mat_<int>;
+}
+
+namespace {
+    template<typename Set1, typename Set2>
+    bool set_intersects(const Set1 & set1, const Set2 &set2) {
+        for(const auto &entry : set1) {
+            if(set2.find(entry) != set2.end())
+                return true;
+        }
+        return false;
+    }
 }
 
 void misaxx_kidney_glomeruli::segmentation3d_klingberg::work() {
@@ -28,7 +40,8 @@ void misaxx_kidney_glomeruli::segmentation3d_klingberg::work() {
 
         std::cout << "Found " << max_label << " glomeruli in layer "<< std::to_string(i) << "\n";
 
-        if(labels.size() > 0) {
+        if(!labels.empty()) {
+            // All connections from this layer -> labels of last layer
             std::unordered_map<int, std::unordered_set<int>> connections;
 
             // Look for connections to the last layer if available
@@ -50,53 +63,17 @@ void misaxx_kidney_glomeruli::segmentation3d_klingberg::work() {
                 }
             }
 
-
-            // Apply re-naming of previous layers (if multiple have been found)
-            {
-                // Obtain a renaming map
-                std::unordered_map<int, int> renaming_map;
-                for(auto &kv : connections) {
-                    if(kv.second.size() > 1) {
-                        int target = *kv.second.begin();
-                        for(int source : kv.second) {
-                            renaming_map[source] = target;
-                        }
-
-                        // Rename for the current layer connection
-                        kv.second = { target };
-                    }
-                }
-
-                // Go through last layers and rename
-                for(size_t j = 0; j < labels.size() - 1; ++j) {
-                    cv::images::labels &last_label = labels[j];
-                    for(int y = 0; y < last_label.rows; ++y) {
-                        int *row = last_label[y];
-                        for(int x = 0; x < label.cols; ++x) {
-                            if(row[x] > 0) {
-                                auto it = renaming_map.find(row[x]);
-                                if(it != renaming_map.end()) {
-                                    row[x] = it->second;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
             // Go through the current layer and rename to target / create new groups
+            // The first pass ignores merging objects, but puts the current layer into the
+            std::unordered_map<int, int> first_pass_renaming;
             {
-                std::unordered_map<int, int> renaming_map;
                 for(auto &kv : connections) {
-                    if (kv.second.size() == 1) {
-                        renaming_map[kv.first] = *kv.second.begin();
-                    }
-                    else if(kv.second.empty()) {
+                    if (kv.second.empty()) {
                         ++global_max_label;
-                        renaming_map[kv.first] = global_max_label;
+                        first_pass_renaming[kv.first] = global_max_label;
                     }
                     else {
-                        throw std::runtime_error("Incomplete layer assignment!");
+                        first_pass_renaming[kv.first] = *kv.second.begin();
                     }
                 }
 
@@ -104,20 +81,83 @@ void misaxx_kidney_glomeruli::segmentation3d_klingberg::work() {
                     int *row = label[y];
                     for(int x = 0; x < label.cols; ++x) {
                         if(row[x] > 0) {
-                            row[x] = renaming_map.at(row[x]);
+                            row[x] = first_pass_renaming.at(row[x]);
                         }
                     }
                 }
             }
 
+            // Move the first pass results into the current buffer
+            labels.emplace_back(std::move(label));
+            ++loaded_label_count;
+
+            // First-pass renaming does not merge objects
+            // We need a second renaming pass that merges all connected objects
+            {
+                // Find the connected components of connections
+                std::vector<std::unordered_set<int>> components;
+                for(const auto &kv : connections) {
+                    if(kv.second.size() > 1)
+                        components.push_back(kv.second);
+                }
+
+                bool changed;
+                do {
+                    changed = false;
+                    for(size_t j = 0; j < components.size(); ++j) {
+                        for(size_t k = 0; k < components.size(); ++k) {
+                            if(j != k) {
+                                if(set_intersects(components.at(j), components.at(k))) {
+                                    // Copy the labels from k to j
+                                    std::copy(components.at(k).begin(), components.at(k).end(),
+                                            std::inserter(components.at(j), components.at(j).begin()));
+
+                                    // Remove k
+                                    std::swap(components.at(k), components.at(components.size() - 1));
+                                    components.resize(components.size() - 1);
+
+                                    changed = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                while(changed);
+
+                // Create a second pass renaming
+                std::unordered_map<int, int> second_pass_renaming;
+                for(const auto &component : components) {
+                    int target = *component.begin();
+                    for(int src : component) {
+                        second_pass_renaming[src] = target;
+                    }
+                }
+
+                // Go through last layers and rename
+                // This includes the current layer
+                for(size_t j = first_loaded_label_index; j < labels.size(); ++j) {
+                    cv::images::labels &current_label = labels[j];
+
+                    for(int y = 0; y < current_label.rows; ++y) {
+                        int *row = current_label[y];
+                        for(int x = 0; x < current_label.cols; ++x) {
+                            if(row[x] > 0) {
+                                auto it = second_pass_renaming.find(row[x]);
+                                if(it != second_pass_renaming.end()) {
+                                    row[x] = it->second;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
         else {
             // Set global max label to current glomeruli count
             global_max_label = max_label;
+            labels.emplace_back(std::move(label));
+            ++loaded_label_count;
         }
-
-        labels.emplace_back(std::move(label));
-        ++loaded_label_count;
 
         while(loaded_label_count > limsize) {
             module->m_output_segmented3d.at(first_loaded_label_index).access_write().set(std::move(labels.at(first_loaded_label_index)));
